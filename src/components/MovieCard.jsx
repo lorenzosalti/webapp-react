5bd94af6e10f2acff08c0952bd8f73b1ac6d0c41
lorenzosalti <lorenzosalti@gmail.com>
2025-05-13 08:45:58 +0200

    fix: invalid dom property class

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ function MovieCard({ data }) {
       <img src={data.image} className="card-img-top" alt={data.title} />
       <div className="card-body">
         <h5 className="card-title">{data.title}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary">{data.director}</h6>
+        <h6 className="card-subtitle mb-2 text-body-secondary">{data.director}</h6>
         <p className="card-text">{data.abstract}</p>
         <a href="#" className="btn btn-primary">Movie Detail</a>
       </div>
