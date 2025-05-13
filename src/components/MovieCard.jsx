57bae173697ba1211a63731aa4127a4901c3d27d
lorenzosalti <lorenzosalti@gmail.com>
2025-05-13 15:24:34 +0200

    add: average vote

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,17 +2,18 @@
 
 function MovieCard({ data }) {
 
-  const { id, title, director, abstract, image } = data
+  const { id, title, director, abstract, image, average_vote } = data
 
   return (
 
 
     <div className="card">
-      <img src={image} className="card-img-top" alt={title} />
+      <img src={image} className="card-img-top mb-2" alt={title} />
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
+        <h5 className="card-title mb-2">{title}</h5>
         <h6 className="card-subtitle mb-2 text-body-secondary">{director}</h6>
-        <p className="card-text">{abstract}</p>
+        <p className="card-text mb-2">{abstract}</p>
+        <h6 className="card-subtitle mb-2">{average_vote}</h6>
         <a href="#" className="btn btn-primary">Movie Detail</a>
       </div>
     </div>
