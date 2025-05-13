fe840daa7d61986c35647f29091110306988d5f6
lorenzosalti <lorenzosalti@gmail.com>
2025-05-13 08:48:45 +0200

    feat: data destructuring

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,15 +2,17 @@
 
 function MovieCard({ data }) {
 
+  const { id, title, director, abstract, image } = data
+
   return (
 
 
     <div className="card">
-      <img src={data.image} className="card-img-top" alt={data.title} />
+      <img src={image} className="card-img-top" alt={title} />
       <div className="card-body">
-        <h5 className="card-title">{data.title}</h5>
-        <h6 className="card-subtitle mb-2 text-body-secondary">{data.director}</h6>
-        <p className="card-text">{data.abstract}</p>
+        <h5 className="card-title">{title}</h5>
+        <h6 className="card-subtitle mb-2 text-body-secondary">{director}</h6>
+        <p className="card-text">{abstract}</p>
         <a href="#" className="btn btn-primary">Movie Detail</a>
       </div>
     </div>
