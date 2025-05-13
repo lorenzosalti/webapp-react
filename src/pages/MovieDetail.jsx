b83eaf8ced9c3419ea0694ced28a145647870545
lorenzosalti <lorenzosalti@gmail.com>
2025-05-13 16:56:23 +0200

    feat: movieDetail render

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,11 +1,47 @@
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import ReviewCard from "../components/ReviewCard"
 
 
 function MovieDetailPage() {
 
+  const { id } = useParams()
+  const [movie, setMovie] = useState({})
+
+
+
+  const movieUrl = `http://localhost:3000/movies/${id}`
+
+  function getMovie() {
+    axios.get(movieUrl)
+      .then(res => {
+        // console.log(res.data)
+        setMovie(res.data)
+      })
+      .catch(err => console.error(err))
+  }
+
+  useEffect(getMovie, [id])
+
+
+  const { title, director, abstract, average_vote, reviews } = movie
+  console.log(reviews)
+
   return (
-    <div>
-      Movie Detail
-    </div>
+    <>
+      <section className="container">
+        <h2>{title}</h2>
+        <h4>Director: {director}</h4>
+        <p>{abstract}</p>
+        <h6>{average_vote}</h6>
+      </section>
+
+      <section className="container">
+        <h3 className="mb-2">Reviews</h3>
+        {reviews ? reviews.map(review => <ReviewCard data={review} key={review.id} />) : <div>Loading</div>}
+      </section>
+    </>
   )
 
 }
