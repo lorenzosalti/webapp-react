f3b57d9cb86c6e6c35b674e4313f421f405b46d7
lorenzosalti <lorenzosalti@gmail.com>
2025-05-13 08:58:51 +0200

    add: header tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import { NavLink } from "react-router-dom"
 function Header() {
 
   return (
-    <nav>
-      <ul>
-        <li><NavLink to='/'>Home Page</NavLink></li>
-        <li><NavLink to='/movies'>Movies</NavLink></li>
-      </ul>
-    </nav>
+    <header className="mb-2">
+      <nav className="p-2">
+        <ul>
+          <li><NavLink to='/'>Home Page</NavLink></li>
+          <li><NavLink to='/movies'>Movies</NavLink></li>
+        </ul>
+      </nav>
+    </header>
   )
 
 }
