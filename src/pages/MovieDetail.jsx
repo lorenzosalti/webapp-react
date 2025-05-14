d243e9ad34699aa0a1eddbd96b848c094b663f2e
lorenzosalti <lorenzosalti@gmail.com>
2025-05-14 15:43:00 +0200

    add: new review form layout

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -40,6 +40,39 @@ function MovieDetailPage() {
       <section className="container">
         <h3 className="mb-2">Reviews</h3>
         {reviews ? reviews.map(review => <ReviewCard data={review} key={review.id} />) : <div>Loading</div>}
+
+
+
+        <div className="card">
+          <div className="card-header">
+            New Review
+          </div>
+          <div className="card-body">
+
+            <div className="mb-3">
+              <label htmlFor="new-review-name" className="form-label">Name</label>
+              <input type="text" className="form-control" id="new-review-name" placeholder="Your Name" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="new-review-vote" className="form-label">Vote</label>
+              <select className="form-select" id="new-review-vote" aria-label="Movie Vote">
+                <option defaultValue="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="new-review-text" className="form-label">Text</label>
+              <textarea className="form-control" id="new-review-text" placeholder="Insert Review" />
+            </div>
+
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+
+          </div>
+        </div>
+
       </section>
     </>
   )
